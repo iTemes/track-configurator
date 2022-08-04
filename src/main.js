68e53eed5c44953a5c4ebcde6894f0856c316428
Artem Aksentenko <akstemes@gmail.com>
2022-08-04 17:55:07 +0300

    i18n fix

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import i18n from "./locales/i18n";
+import App from "@/App.vue";
+import router from "@/router";
+import store from "@/store";
+import i18n from "@/locales/i18n";
 
-import "./styles/scss/main.scss";
+import "@/styles/scss/main.scss";
 
 createApp(App).use(i18n).use(store).use(router).mount("#app");
